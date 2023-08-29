2d803b09c586e35df6d737fcdf8fe56f5dac1664
vinayak soni <vianyaksoni977001@gmail.com>
2023-08-29 22:26:13 +0530

    update connection console

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 export const connectDB =()=> { 
    mongoose.connect(process.env.MONGO_URL,{
    dbName:"todoApp",
-}).then(()=>console.log("DB connected"))
+}).then(()=>console.log(`DB connected  ${process.env.MONGO_URL}`))
 .catch((e)=>console.log(e));
 }
